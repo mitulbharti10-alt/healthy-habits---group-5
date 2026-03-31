79d35bba3fffe884e192c3637ec55795fd358ec9
tanish.nadikota981@education.nsw.gov.au <tanish.nadikota981@education.nsw.gov.au>
2026-03-31 03:18:46 +0000

    Created LoginPage; Edited settings; Deleted Form1

---
+++ client_code/LoginPage/__init__.py
@@ -0,0 +1,15 @@
+from ._anvil_designer import LoginPageTemplate
+from anvil import *
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class LoginPage(LoginPageTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+anvil.users.login_with_form()
+
