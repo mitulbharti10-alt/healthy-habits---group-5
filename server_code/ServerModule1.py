4726d411afd1cbb551a3afec1bbd9ae99d875efb
mitul.bharti@education.nsw.gov.au <mitul.bharti@education.nsw.gov.au>
2026-04-13 09:29:54 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -6,17 +6,10 @@ from anvil.tables import app_tables
 import anvil.server
 
 @anvil.server.callable
+def create_goal (user, title, subject, goal_type, target_value):
+  app 
 
 
-# This is a server module. It runs on the Anvil server,
-# rather than in the user's browser.
-#
-# To allow anvil.server.call() to call functions here, we mark
-# them with @anvil.server.callable.
-# Here is an example - you can replace it with your own:
-#
-# @anvil.server.callable
-# def say_hello(name):
-#   print("Hello, " + name + "!")
-#   return 42
-#
+
+
+
