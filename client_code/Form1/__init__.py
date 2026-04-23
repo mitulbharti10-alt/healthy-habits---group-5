780a17fa3916716e387b3d9d1b28ae9dcb56fed8
mitul.bharti@education.nsw.gov.au <mitul.bharti@education.nsw.gov.au>
2026-04-23 23:04:57 +0000

    Created Form1; Edited settings, ServerModule1, Dashboard and LoginPage

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,23 +1,18 @@
-from ._anvil_designer import DashboardTemplate
+from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
 import anvil.facebook.auth
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
-import anvil.server
 import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class Dashboard(DashboardTemplate):
+class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-
-  @handle("G", "click")
-  def G_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    pass  # Write Code Here
