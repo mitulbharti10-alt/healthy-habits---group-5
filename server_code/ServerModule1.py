2ab46ab83bea38ab837e89d0a69fbfa39c52bbb0
mitul.bharti@education.nsw.gov.au <mitul.bharti@education.nsw.gov.au>
2026-04-23 22:52:55 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -13,13 +13,14 @@ def get_goal_progress(goal_row): # fuction code for the goal tracking feature
   tasks = app_tables.tasks.tasks.search(goal=goal_row) # finds the specific goal
   total = len(lists(tasks)) 
   completed = lens ({})
+  
 
 
 @anvil.server.callable
 def update_user_profile(name):
   user = anvil.users.get_user()
   if user:
-    user['name'] = name # Assumes you added a 'name' column to Users table
+    user['name'] = name # Assumes you added a 'name' column to Users table 
 
 
 
