0c8a638b26b54581095a70c8b8fa496acc0c6705
tanish.nadikota981@education.nsw.gov.au <tanish.nadikota981@education.nsw.gov.au>
2026-04-23 03:08:36 +0000

    Edited ServerModule1, Dashboard and LoginPage

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,23 +1,20 @@
-import anvil.google.auth, anvil.google.drive, anvil.google.mail
-from anvil.google.drive import app_files
 import anvil.users
-import anvil.email
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 
 @anvil.server.callable
-def create_goal (user, title, subject, goal_type, target_value):
-  app_tables.goals.add_row(
-    user=user, 
-    title=title,
-    subject=subject, 
-    goal_type=goal_type,
-    target_value=target_value, 
-    current_value=0
-    created_data=datetime.now()   
-  )  
+def get_current_user_info():
+  # This returns the current user row or None
+  return anvil.users.get_user()
+
+@anvil.server.callable
+def update_user_profile(name):
+  user = anvil.users.get_user()
+  if user:
+    user['name'] = name # Assumes you added a 'name' column to Users table
+
 
   
 
