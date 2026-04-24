5138f36250677eecc322f3b0181fd8c1bb615c90
mitul.bharti@education.nsw.gov.au <mitul.bharti@education.nsw.gov.au>
2026-04-24 11:42:13 +0000

    Created Settings; Edited Dashboard; Deleted Form1

--- client_code/Dashboard/__init__.py
+++ client_code/Dashboard/__init__.py
@@ -17,7 +17,10 @@ class Dashboard(DashboardTemplate):
 
     # Any code you write here will run before the form opens.
 
-  @handle("G", "click")
-  def G_click(self, **event_args):
+  @handle("Goals", "click")
+  def Goals_click(self, **event_args):
     """This method is called when the button is clicked"""
     pass  # Write Code Here
+
+    self.goals = 100 
+    
