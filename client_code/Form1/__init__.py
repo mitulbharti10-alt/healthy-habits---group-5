a4e1a8b35fbc05c5b6d2a25c1c9f9e30d386f476
mitul.bharti@education.nsw.gov.au <mitul.bharti@education.nsw.gov.au>
2026-04-30 08:17:10 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -19,5 +19,25 @@ class Form1(Form1Template):
       # This sends the percentage (e.g. 75) to your Javascript function 
       self.call_js('setProgress', percentage)  
 
-Class Form1 
-      
+  @handle("", "show")
+  def form_show(self, **event_args):
+    """This method is called when the form is shown on the page"""
+    Class Form1
+def _init_(self, **properties):
+  # Set Form properties and Data Binding. 
+  self.init_components(**properties) 
+
+  # Set initial progress to 75%
+  self.call_js('setProgress', 85) 
+
+Class Form1
+def _init_(self, **properties):
+  # Set Form properties and Data Binding. 
+  self.init_components(**properties) 
+
+  # Set initial progress to 75%
+  self.call_js('setProgress', 85) 
+  
+  
+
+
