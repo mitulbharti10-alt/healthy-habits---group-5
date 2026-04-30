eb9d0859651f964d41d40bd3853da6db3c039693
mitul.bharti@education.nsw.gov.au <mitul.bharti@education.nsw.gov.au>
2026-04-30 08:54:50 +0000

    Edited Circle-progress.js, settings, Subject_Card.html and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -9,34 +9,20 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-
-class Form1(Form1Template):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
-
-    def update_progress(self, perrcentage) : 
-      # This sends the percentage (e.g. 75) to your Javascript function 
-      self.call_js('setProgress', percentage)  
-
-  @handle("", "show")
-  def form_show(self, **event_args):
-    """This method is called when the form is shown on the page"""
-    Class Form1
-def _init_(self, **properties):
-  # Set Form properties and Data Binding. 
-  self.init_components(**properties) 
-
-  # Set initial progress to 75%
-  self.call_js('setProgress', 85) 
-
-Class Form1
-def _init_(self, **properties):
-  # Set Form properties and Data Binding. 
-  self.init_components(**properties) 
-
-  # Set initial progress to 75%
-  self.call_js('setProgress', 85) 
+class Form1(Form1Template):  
+  def __init__(self, **properties): 
+    # Set Form properties and Data Binding. 
+    self.init_components(**properties) 
+  
+  def update_progress(self,percentage): 
+    # This sends the percentage to your Javascript function
+    self.call_js('setProgress', percentage) 
+    
+  
+  def form_show(self, **event_args): 
+    """This method is called when the form is shown on the page""" 
+  # Set initial progress to 00000085% 
+    self.update_progress(85) 
   
   
 
