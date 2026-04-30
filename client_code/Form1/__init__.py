c7d27e358a0d2df1e8d635bc3ac8500d0c0f7dff
mitul.bharti@education.nsw.gov.au <mitul.bharti@education.nsw.gov.au>
2026-04-30 03:21:26 +0000

    Edited Form1; Deleted Form2

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -9,6 +9,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
+
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
