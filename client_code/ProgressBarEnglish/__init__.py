6d79887105bd1048655a550f68aa102f480c1d18
mitul.bharti@education.nsw.gov.au <mitul.bharti@education.nsw.gov.au>
2026-05-02 00:15:12 +0000

    Created ProgressBarScience and ProgressBarEnglish; Edited Dashboard; Deleted Form3

--- client_code/ProgressBarEnglish/__init__.py
+++ client_code/ProgressBarEnglish/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import Form3Template
+from ._anvil_designer import ProgressBarEnglishTemplate
 from anvil import *
 import anvil.server
 import anvil.facebook.auth
@@ -10,7 +10,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class Form3(Form3Template):
+class ProgressBarEnglish(ProgressBarEnglishTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
