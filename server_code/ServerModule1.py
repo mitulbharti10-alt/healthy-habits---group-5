b0d0000778da016e50c4f7133d5406ce087bfa9c
mitul.bharti@education.nsw.gov.au <mitul.bharti@education.nsw.gov.au>
2026-05-02 06:12:05 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -23,6 +23,30 @@ def update_user_profile(name):
   if user:
     user['name'] = name # Assumes you added a 'name' column to Users table 
 
+@anvil.server.callable 
+def record_login(): 
+  user = anvil.users.get_user() 
+  if user: 
+    today = date.today() 
+    # Checks for if a login for today already exists. 
+    existing = app_tables.loginhistory.get(user=user, login_date=today) 
+    if not existing: 
+      app_tables.loginhistory.add_row(user=user, login_date=today) 
+
+@anvil.server.callable
+def get_streak_data(): 
+  user = anvil.users.get_user() 
+  if not user: 
+    return[] 
+
+today = date.today() 
+# Get last 7 days (including today) 
+week_dates = [today - timedelta(days=i) for i in range(6, -1, -1)] 
+
+stats = [] 
+for d in week_dates: 
+  logged = app.tables.loginhistory 
+
 
 
 
