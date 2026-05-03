08a966612a387d1af9a051f2426a47f6082cd594
mitul.bharti@education.nsw.gov.au <mitul.bharti@education.nsw.gov.au>
2026-05-03 11:32:11 +0000

    Edited Daycard

--- client_code/Daycard/__init__.py
+++ client_code/Daycard/__init__.py
@@ -15,18 +15,22 @@ class Daycard(DaycardTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-   # 'self.item' will contain the data for one day
+    # 'self.item' will contain the data for one day
 
-if self.item: 
-  # set the number on the card 
-  self.label_date.text = self.item['date'].day 
+    if self.item: 
+      # set the number on the card 
+      self.label_date.text = self.item['date'].day 
 
-  # Change colour based on if the user logged in ('active') 
-  if self.item['active']: 
-    self.background = "#2ecc71" #Emrald Green
-    self.label_date.foreground = "white" 
-  else: 
-    
-    
+    # Change colour based on if the user logged in ('active') 
+    if self.item['active']: 
+      self.background = "#2ecc71" #Emrald Green
+     self.label_date.foreground = "white" 
+    else: 
+     self.background = "#eeeeee"  # Light Grey 
+     self.label_date.foreground = "#777777"
 
-    
+
+
+
+
+
