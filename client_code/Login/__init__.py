c1e531369910e29e5b40a2f860c965fd79e59702
tanish.nadikota981@education.nsw.gov.au <tanish.nadikota981@education.nsw.gov.au>
2026-05-03 11:39:49 +0000

    Edited settings, Login and Dashboard

--- client_code/Login/__init__.py
+++ client_code/Login/__init__.py
@@ -1,18 +1,17 @@
 from ._anvil_designer import LoginTemplate
 from anvil import *
-import anvil.server
-import anvil.facebook.auth
-import anvil.google.auth, anvil.google.drive
-from anvil.google.drive import app_files
 import anvil.users
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
 
-
-class Login(LoginTemplate):
+class Login(LoginTemplate): # This line MUST match the name in the sidebar
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    anvil.users.login_with_form()
-    # Any code you write here will run before the form opens.
+
+    # Show the login form
+    user = anvil.users.login_with_form()
+
+    if user:
+      open_form('EditProfile')
+    else:
+      Notification("Please log in to continue.").show()
+
