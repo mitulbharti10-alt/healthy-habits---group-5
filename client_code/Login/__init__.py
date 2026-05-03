599d1e1656ca2bc6c919050553106231371b4b70
tanish.nadikota981@education.nsw.gov.au <tanish.nadikota981@education.nsw.gov.au>
2026-05-03 11:32:41 +0000

    Edited Login

--- client_code/Login/__init__.py
+++ client_code/Login/__init__.py
@@ -1,15 +1,28 @@
 from ._anvil_designer import LoginPageTemplate
 from anvil import *
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.server
 import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 class LoginPage(LoginPageTemplate):
   def __init__(self, **properties):
     self.init_components(**properties)
 
-    # Using signup_archived_fields lets new users enter their name during sign-up
+    # 1. Show the login form
+    # signup_archived_fields allows new users to enter their name
+    user = anvil.users.login_with_form(signup_archived_fields={'username': 'Full Name'})
 
-    # 2. Check if login was successful
+    # 2. Redirect to EditProfile only if login is successful
     if user:
-      # REDIRECT: Go straight to the EditProfile form
-      open_form('EditProfile') 
+      open_form('EditProfile')
+    else:
+      # Optional: message if they close the popup without logging in
+      Notification("Login required to continue.").show()
+
+
+
 
