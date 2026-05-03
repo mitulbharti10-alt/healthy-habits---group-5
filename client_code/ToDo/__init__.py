dac817920ff95bd9ef46c4e445d1fdc8f6f3906c
tanish.nadikota981@education.nsw.gov.au <tanish.nadikota981@education.nsw.gov.au>
2026-05-03 09:10:29 +0000

    Created ToDo; Edited settings, Dashboard and EditProfile; Deleted CustomLogin and To_Do

--- client_code/ToDo/__init__.py
+++ client_code/ToDo/__init__.py
@@ -1,15 +1,15 @@
-from ._anvil_designer import To_DoTemplate
+from ._anvil_designer import ToDoTemplate
 from anvil import *
+import anvil.server
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
-import anvil.server
 import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class To_Do(To_DoTemplate):
+class ToDo(ToDoTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
