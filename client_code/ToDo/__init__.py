f65fa8c715394b24935035195efd378453a6151f
tanish.nadikota981@education.nsw.gov.au <tanish.nadikota981@education.nsw.gov.au>
2026-05-04 11:05:11 +0000

    Edited ToDo

--- client_code/ToDo/__init__.py
+++ client_code/ToDo/__init__.py
@@ -17,3 +17,17 @@ def __init__(self, **properties):
   self.init_components(**properties)
   # Fill your list with data from the server
   self.task_list.items = anvil.server.call('get_tasks')
+
+  def add_btn_click(self, **event_args):
+    # 1. Grab what the user typed
+    new_task = self.new_task_box.text
+
+    if new_task:
+      # 2. Send it to the server to save in the database
+      anvil.server.call('add_task', new_task)
+
+      # 3. Clear the box so they can type again
+      self.new_task_box.text = ""
+
+      # 4. Refresh the list to show the new task
+      self.refresh_tasks()
