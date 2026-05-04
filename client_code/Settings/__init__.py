d68d466a14ab4a96dc2a461076623c7da6a15d64
tanish.nadikota981@education.nsw.gov.au <tanish.nadikota981@education.nsw.gov.au>
2026-05-04 11:58:38 +0000

    Created Settings; Edited settings, ToDo and FakeDashboard

--- client_code/Settings/__init__.py
+++ client_code/Settings/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import FakeDashboardTemplate
+from ._anvil_designer import SettingsTemplate
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -9,14 +9,9 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class FakeDashboard(FakeDashboardTemplate):
+class Settings(SettingsTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-
-  @handle("button_1", "click")
-  def button_1_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    open_form('Settings')
